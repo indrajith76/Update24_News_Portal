0777e84ab8c100b3c53c37322ffcfe186bc5d872
indrajith76 <indrajithgoswami@gmail.com>
2022-09-03 04:03:53 +0600

    added counter section

--- js/index.js
+++ js/index.js
@@ -16,11 +16,37 @@ const displayCategory = categories => {
     categories.forEach(category => {
         const categoryDiv = document.createElement('div');
         categoryDiv.innerHTML = `
-            <a onclick="" class="text-base font-bold text-gray-500 hover:text-blue-600 hover:bg-blue-300 hover:px-2 hover:py-1 hover:rounded" href="#">${category.category_name}</a>
+            <a onclick="loadCategoryNews('${category.category_id}')" class="text-base font-bold text-gray-500 hover:text-blue-600 hover:bg-blue-300 hover:px-2 hover:py-1 hover:rounded" href="#">${category.category_name}</a>
         `;
         categorySection.appendChild(categoryDiv);
-        console.log(category);
     });
 }
+
+const loadCategoryNews = async categoryId => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayNewsInfo(data.data);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+const displayNewsInfo = allNewsInfo => {
+    const newsCounter = document.getElementById('counter');
+    if(allNewsInfo.length !== 0) {
+        newsCounter.innerText = allNewsInfo.length;
+    }
+    else {
+        newsCounter.innerText = 'NO';
+    }
+    allNewsInfo.forEach(allInfo => {
+        
+    });
+}
+
+loadCategoryNews('01')
 
 loadCategory()
