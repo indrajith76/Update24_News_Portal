428db5240424331c27130bb4f454684d0cc39677
indrajith76 <indrajithgoswami@gmail.com>
2022-09-03 10:00:28 +0600

    modal added

--- js/index.js
+++ js/index.js
@@ -23,6 +23,9 @@ const displayCategory = categories => {
 }
 
 const loadCategoryNews = async categoryId => {
+    //start loader
+    spinner(true);
+
     const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
     try {
         const res = await fetch(url);
@@ -42,11 +45,103 @@ const displayNewsInfo = allNewsInfo => {
     else {
         newsCounter.innerText = 'NO';
     }
+
+    const newsCardContainer = document.getElementById('news-card-container');
+    newsCardContainer.innerHTML = ``;
     allNewsInfo.forEach(allInfo => {
-        
+        const newsCardsDiv = document.createElement('div');
+        newsCardsDiv.classList.add('news-card')
+        newsCardsDiv.innerHTML = `
+            <div onclick="loadModal('${allInfo._id}')" class="card-item flex gap-5 shadow-lg p-2 rounded-lg border">
+                <div class="w-3/12">
+                    <img class="h-full" src='${allInfo.image_url}'>
+                </div>
+                <div class="w-3/4">
+                    <h3 class="text-xl font-semibold mb-2">${allInfo.title}</h3>
+                    <p class="mb-3">${allInfo.details.slice(0, 300)}...</p>
+                    
+                    <div class="flex justify-between">
+                        <div class="flex gap-3">
+                            <img class="rounded-full h-12" src="${allInfo.author.img}">
+                            <div>
+                                <h5 class="text-lg font-semibold">${allInfo.author.name ? allInfo.author.name : "Not Available."}</h5>
+                                <p class="font-medium text-slate-500">${allInfo.author.published_date ? allInfo.author.published_date.slice(0, 10):'not available.'}</p>
+                            </div>
+                        </div>
+                        <div class="flex items-center gap-2">
+                            <i class="fa-regular fa-eye"></i>
+                            <h5>${allInfo.total_view ? allInfo.total_view : "Not available."}</h5>
+                        </div>
+                        <div class="flex items-center gap-1">
+                            <i class="fa-regular fa-star-half-stroke"></i>
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                        </div>
+                        <div class="flex items-center mr-5">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        newsCardContainer.appendChild(newsCardsDiv);
     });
+    spinner(false);
 }
+
+
+const loadModal = async newsId => {
+    const url = `https://openapi.programming-hero.com/api/news/${newsId}`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayModalData(data.data[0]);
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+
+const modalOverlay = document.getElementById('modal-overlay');
+const displayModalData = data => {
+    modalOverlay.classList.remove('hidden');
+    const modalBody = document.getElementById('modal-body');
+    modalBody.innerHTML = `
+        <h1>${data.title}</h1>
+        <div class="flex gap-3">
+            <img class="w-11 rounded-full" src="${data.author.img}">
+            <div>
+                <h4>${data.author.name}</h4>
+                <p>${data.author.published_date}</p>
+            </div>
+        </div>
+        <img src="${data.image_url}">
+        <p>${data.details}</p>
+        <i onclick="modalOff()" class="absolute top-1 right-1 cursor-pointer fa-solid fa-xmark"></i>
+    `;
+}
+
+const modalOff = () => {
+    modalOverlay.classList.add('hidden');
+}
+
+const spinner = data => {
+    const spinnerDiv = document.getElementById('spinner');
+    if(data === true){
+        spinnerDiv.classList.remove('hidden');
+        spinnerDiv.classList.add('flex');
+    }
+    else {
+        spinnerDiv.classList.remove('flex');
+        spinnerDiv.classList.add('hidden');
+    }
+}
+
 
 loadCategoryNews('01')
 
 loadCategory()
+// spinner(true)
